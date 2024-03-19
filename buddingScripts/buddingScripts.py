eaf00318f71969f99c92a74afda4fbdeec9f1589
Xiuqi Pan <xp76@cornell.edu>
2024-03-19 10:53:46 -0400

    Add console script

---
+++ buddingScripts/buddingScripts.py
@@ -0,0 +1,77 @@
+from buddingScripts import script_generator
+from buddingScripts.exceptions import cli_input_error
+from pathlib import Path
+import argparse
+import json
+import os
+
+def main():
+    # setting up parsers
+    parser = argparse.ArgumentParser('buddingScripts')
+    with open(os.path.join(Path.home(), '.buddingScriptsRC.json'), 'r') as f:
+        settings = json.load(f)
+
+    subcommandParsers = parser.add_subparsers(title="buddingScripts",
+                                            dest="subcommand", )
+
+    script_generators_settings = settings['script_generators']
+    template_sub_dict = settings['templates']
+    generator_parsers = dict()
+    generators = dict()
+
+    for generator_name, generator_settings in script_generators_settings.items():
+        sub_parser = subcommandParsers.add_parser(generator_name)
+        generator_parsers[generator_name] = sub_parser
+
+        generator = script_generator(generator_name, generator_settings, template_sub_dict)
+        generators[generator_name] = generator
+
+        generator.set_argparser(sub_parser)
+
+        sub_parser.add_argument('--opath', '-O', 
+                                dest='opath', 
+                                help='Scripts output path.', 
+                                default='.')
+        
+        sub_parser.add_argument('--template',
+                                dest='template', 
+                                help='Template path.', 
+                                default=False)
+
+    args = parser.parse_args()
+    generator = generators[args.subcommand]
+
+    if args.template:
+        with open(args.template, 'r') as f:
+            template_content = f.read()
+        generator.load_template(template_content)
+
+    # expansion mode
+    if args.job_name.__str__()[-5:] == '.list':
+        with open(args.job_name, 'r') as f:
+            job_names = [s.replace('\n', '') for s in f.readlines()]
+        
+        num_jobs = len(job_names)
+        arg_list_dict = {'job_name': job_names}
+
+        for job_name in job_names:
+            for key, value in args.__dict__.items():
+                if value.__str__()[-5:] == '.list':
+                    with open(value, 'r') as f:
+                        arg_list_dict[key] = [s.replace('\n', '') for s in f.readlines()]
+                    # make sure lists are of the same length
+                    # if not, raise error
+                    if len(arg_list_dict[key]) != num_jobs:
+                        raise cli_input_error("lists' length not match")
+                else:
+                    arg_list_dict[key] = [value] * num_jobs
+
+        for i in range(num_jobs):
+            arg_dict = {key: value[i] for key,value in arg_list_dict.items()}
+
+            with open(os.path.join(args.opath, arg_dict['job_name'] + settings['suffix'][args.subcommand.__str__()]), 'w') as f:
+                f.write(generator.render(arg_dict))
+    else:
+        with open(os.path.join(args.opath, args.job_name + settings['suffix'][args.subcommand.__str__()]), 'w') as f:
+            f.write(generator.render(args.__dict__))
+
